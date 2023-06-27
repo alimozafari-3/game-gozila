a66c438a07d1fe38e14c98d4be7f8bec4d5b8d83
ali <ali.mozafari733@yahoo.com>
2023-06-27 23:49:00 +0430

    add function jump

--- gozila.js
+++ gozila.js
@@ -1,1 +1,14 @@
+var character = document.getElementById("character");
+var block = document.getElementById("block");
+let wi = document.querySelector(".boht");
 
+var conter = 0;
+function jump() {
+  if (character.classList == "animate") {
+    return;
+  }
+  character.classList.add("animate");
+  setTimeout(function () {
+    character.classList.remove("animate");
+  }, 300);
+}
