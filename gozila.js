5e59422de642747558551616b841c485d9b30eb2
ali <ali.mozafari733@yahoo.com>
2023-06-28 21:22:30 +0430

    add checkdead and charactertop and blockleft js

--- gozila.js
+++ gozila.js
@@ -12,3 +12,22 @@ function jump() {
     character.classList.remove("animate");
   }, 300);
 }
+
+var checkdead = setInterval(function () {
+  let charactertop = parseInt(
+    window.getComputedStyle(character).getPropertyValue("top")
+  );
+
+  let blockleft = parseInt(
+    window.getComputedStyle(block).getPropertyValue("left")
+  )
+  if (blockleft < 20 && blockleft > -20 && charactertop >= 130) {
+    block.style.animation = "none";
+    alert("game over" + Math.floor(conter / 100));
+    conter = 0;
+    block.style.animation = "block 1s infinite linear";
+  } else {
+    conter++;
+    document.getElementById("scorespan").innerHTML = Math.floor(conter / 100);
+  }
+}, 10);
